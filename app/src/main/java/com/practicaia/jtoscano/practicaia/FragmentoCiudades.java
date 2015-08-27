f19ea3e0d60a5b6a0f7f7223f0327be57fc7bf4c
jtoscano <jtoscano@ia.com.mx>
2015-08-27 17:32:09 -0500

    IA Interactive Práctica

--- app/src/main/java/com/practicaia/jtoscano/practicaia/FragmentoCiudades.java
+++ app/src/main/java/com/practicaia/jtoscano/practicaia/FragmentoCiudades.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
